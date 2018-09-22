3e6e50ae41e7d918fd2e74103489fe8799ea07e8
Ravulakolu <bindu.ravulakolu@capgemini.com>
2018-09-22 02:19:06 -0400

    first commit

---
+++ src/main/java/com/capgemini/bankapp/Application.java
@@ -0,0 +1,25 @@
+package com.capgemini.bankapp;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.capgemini.bankapp.config.AppConfig;
+import com.capgemini.bankapp.controller.BankAccountController;
+
+public class Application {
+
+	public static void main(String[] args) throws AccountNotFoundException {
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		/*
+		 * ApplicationContext context = new
+		 * ClassPathXmlApplicationContext("applicationContext.xml");
+		 */
+		BankAccountController bankAccountController = context.getBean("bankAccountController",
+				BankAccountController.class);
+		System.out.println(bankAccountController.getBalance(123455));
+	}
+
+}
---
+++ src/main/java/com/capgemini/bankapp/exception/LowBalancException.java
@@ -0,0 +1,11 @@
+package com.capgemini.bankapp.exception;
+
+
+
+	public class LowBalancException extends Exception {
+
+		public LowBalancException(String string) {
+	super.getMessage();
+		}
+
+	}
